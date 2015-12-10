3c7ca655f4f6e657deb4d4f5792e91459f1b2aaa
Jackson Morgan <jmorgan@stuy.edu>
2015-12-10 13:02:00 -0500

    Made all of the base files

---
+++ Sphere.java
@@ -0,0 +1,13 @@
+public class Sphere extends Circle{
+
+    public Sphere(double radius, String name){
+	super(radius,name);
+    }
+
+    public String toString(){
+	return "FIXME!";
+    }
+}
+
+
+
